9839871c6984e36885978d58e10de6d7dd9a1dca
zenatnoor86 <zenatnoor86@gmail.com>
2025-09-06 22:10:18 +0500

    Update config.js

--- config.js
+++ config.js
@@ -8,5 +8,5 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || 'YASIYA-MD~kdxFBZTa#Gk8zUBoeUkAeIZog4gRRS2G1tzuToKkjSha-MMnMZJA'
+    SESSION_ID: process.env.SESSION_ID || 'YASIYA-MD?u7kp1l'
 };
